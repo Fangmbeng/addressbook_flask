21f1caf3ad48f609265d4d09cc1b21315c33847f
Fangmbeng <yorobeau@gmail.com>
2023-02-20 17:28:59 -0700

    Fangmbeng

--- app/forms.py
+++ app/forms.py
@@ -6,5 +6,5 @@ class Signupform(FlaskForm):
     email = EmailField('Email', validators = [InputRequired()])
     username = StringField('username', validators = [InputRequired()])
     password = PasswordField('password', validators = [InputRequired()])
-    confirm_password = PasswordField('confirm password', validators = [InputRequired(), EqualTo(password)])
+    confirm_password = PasswordField('confirm password', validators = [InputRequired(), EqualTo("password")])
     submit = SubmitField()
